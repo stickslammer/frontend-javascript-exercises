5f6a92023acba7cc0b519b95b9b68ebb67b58e55
Shane Tyler <soniccinema@gmail.com>
2015-06-11 05:56:06 +0000

    New updates sdt

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,20 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+  return ("Hello " + firstName + ",\n\n" + message  + "\n\nSincerely,"  + "\nJoe");
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+  return bigString.substring(7, 10); + " " + bigString.substring( 14, 20);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
+  return text.indexOf(searchString);
 
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+  return text.indexOf(searchString, 10);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+  return text.substring(17, 41);
 };
