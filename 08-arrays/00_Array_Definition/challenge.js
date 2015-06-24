568268af08ce82a299652e21fb641df4c81e1c67
Shane Tyler <soniccinema@gmail.com>
2015-06-24 23:09:11 +0000

    Completed array checkpoint sdt

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,17 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a, b, c, d) {
+  var myNewArray =  new Array(4);
+  myNewArray[0] = a;
+  myNewArray[1] = b;
+  myNewArray[2] = c;
+  myNewArray[3] = d;
+  return myNewArray;
+}
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(myArray) {
+// take one argument, an array with at least 3 elements
+
+// return a new array with the first and last element of the passed array
+
+  return [myArray[0], myArray[myArray.length -1]];
+
+}
